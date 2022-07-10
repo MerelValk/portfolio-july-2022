bfa2951448020837734428dd19a75daa40d8134d
Merel <merel.vandervalk@student.hu.nl>
2022-07-10 16:41:49 +0200

    added arrow svg

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import style from './header.module.css'
+import {ReactComponent as Arrow} from '../../assets/icons/Arrow.svg'
 
 export default function Header() {
   return (
       <div className={style.container}>
         <div className={style.leftContainer}>
-          <h1>Merel van der Valk.</h1>
-          <p>PRODUCT OWNER, DESIGNER &</p>
-          <p>SOFTWARE DEVELOPER</p>
+          <div>
+            <h1>Merel van der Valk.</h1>
+            <p>PRODUCT OWNER, DESIGNER &</p>
+            <p>SOFTWARE DEVELOPER</p>
+          </div>
+          <a className={style.arrowButton}>
+            <Arrow />
+          </a>
         </div>
         <div className={style.rightContainer}>
           <img src="MerelUtrecht.jpg" alt="Merel lachend" className={style.headerImage}/>
